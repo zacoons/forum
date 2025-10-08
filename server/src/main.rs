6f8a50867e482b7ad0979397ccc2d76ad843ac1c
zac <zac@zacoons.com>
2025-10-08 22:20:07 +1000

    Add command parsing/handling to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,39 +1,153 @@
+use anyhow::{Result, anyhow};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use tokio::net::tcp::{ReadHalf, WriteHalf};
+use tokio::net::{TcpListener, TcpStream};
+
+const RW_BUF_SIZE: usize = 512;
+const MAX_MSG_SIZE: usize = 1024;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:7172").await?;
 
     loop {
-        let (mut socket, _) = listener.accept().await?;
-
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
-
-            // In a loop, read data from the socket and write the data back.
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    // socket closed
-                    Ok(0) => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("Failed to read from socket: {:?}", e);
-                        return;
-                    }
-                };
-
-                println!("Got message {}", str::from_utf8(&buf[0..n]).unwrap());
-
-                // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("Failed to write to socket: {:?}", e);
-                    return;
-                }
-                break;
-            }
-        });
+        let (stream, _) = listener.accept().await?;
+        tokio::spawn(handle_client(stream));
     }
 }
 
-async fn handle_auth() {}
+struct StackVec<T, const N: usize> {
+    buf: [T; N],
+    pub len: usize,
+}
+
+impl<T, const N: usize> StackVec<T, N>
+where
+    T: Copy + Sized,
+{
+    pub fn new(buf: [T; N]) -> Self {
+        Self { buf: buf, len: 0 }
+    }
+
+    pub fn as_slice(&self) -> &[T] {
+        &self.buf[..self.len]
+    }
+
+    pub fn append(&mut self, other: &[T]) {
+        self.buf[self.len..(self.len + other.len())].copy_from_slice(other);
+        self.len += other.len();
+    }
+
+    pub fn clear(&mut self) {
+        self.len = 0;
+    }
+}
+
+async fn handle_client(mut stream: TcpStream) {
+    let mut readbuf = [0; RW_BUF_SIZE];
+    let mut cmdbuf = StackVec::new([0; MAX_MSG_SIZE]);
+
+    let (mut reader, mut writer) = stream.split();
+
+    loop {
+        let n = match reader.read(&mut readbuf).await {
+            Ok(0) => {
+                println!("Client disconnected");
+                return;
+            }
+            Ok(n) => n,
+            Err(err) => {
+                eprintln!("Error reading from socket: {:?}", err);
+                return;
+            }
+        };
+        let mut prevcmd = 0;
+        let mut i = 0;
+        while i <= n - 4 {
+            // Upon encountering an end delimiter:
+            if &readbuf[i..i + 4] == b"\0end" {
+                // Finish the cmdbuf and use it to call handle_cmd
+                cmdbuf.append(&readbuf[prevcmd..i]);
+                handle_cmd(&mut reader, &mut writer, &cmdbuf.as_slice()).await;
+
+                // Clear the cmdbuf to store the next command
+                cmdbuf.clear();
+
+                // Store the index into buf where this command ends
+                // so that we know where to start reading the next one
+                prevcmd = i + 4;
+                i += 4;
+            }
+            i += 1;
+        }
+        i -= 1;
+        if i < n {
+            cmdbuf.append(&readbuf[i..n]);
+        }
+    }
+}
+
+async fn handle_cmd<'a>(reader: &mut ReadHalf<'a>, writer: &mut WriteHalf<'a>, cmdbuf: &[u8]) {
+    // At most 8 parts
+    let mut parts = cmdbuf.splitn(8, |&c| c == b'\0');
+
+    if let Some(cmd) = parts.next() {
+        if let Err(err) = match cmd {
+            b"posts" => handle_posts(reader, writer).await,
+
+            b"auth" => handle_auth(reader, writer, parts).await,
+
+            _ => {
+                _ = writer.write_all(b"Invalid command").await;
+                if let Ok(cmdstr) = str::from_utf8(cmd) {
+                    eprintln!("Invalid command: {}", cmdstr);
+                } else {
+                    eprintln!("Invalid command: {:?}", cmd);
+                }
+                return;
+            }
+        } {
+            _ = writer.write_all(err.to_string().as_bytes()).await;
+            eprintln!("{:?}", err);
+            return;
+        }
+    } else {
+        _ = writer.write_all(b"Badly formatted command").await;
+        if let Ok(bufstr) = str::from_utf8(cmdbuf) {
+            eprintln!("Badly formatted command: {}", bufstr);
+        } else {
+            eprintln!("Badly formatted command: {:?}", cmdbuf);
+        }
+        return;
+    }
+}
+
+async fn handle_posts<'a>(reader: &mut ReadHalf<'a>, writer: &mut WriteHalf<'a>) -> Result<()> {
+    writer.write_all(b"post1, post2, ...").await?;
+    writer.flush().await?;
+    Ok(())
+}
+
+async fn handle_auth<'a>(
+    reader: &mut ReadHalf<'a>,
+    writer: &mut WriteHalf<'a>,
+    mut args: impl Iterator<Item = &[u8]>,
+) -> Result<()> {
+    let username = match args.next() {
+        Some(it) => it,
+        None => return Err(anyhow!("Missing username")),
+    };
+    let passwd = match args.next() {
+        Some(it) => it,
+        None => return Err(anyhow!("Missing password")),
+    };
+
+    if username == b"zacoons" && passwd == b"a" {
+        writer.write_all(b"OK").await?;
+    } else {
+        writer.write_all(b"NOAUTH").await?;
+    }
+    writer.flush().await?;
+
+    Ok(())
+}
--- tui/src/main.zig
+++ tui/src/main.zig
@@ -1,28 +1,67 @@
 const std = @import("std");
 const print = std.debug.print;
 
+const rw_buf_size = 512;
+const max_msg_size = 1024;
+
 pub fn main() !void {
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // defer std.debug.assert(gpa.deinit() == .ok);
-    // const alloc = gpa.allocator();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const alloc = gpa.allocator();
+
+    var args = try std.process.argsWithAllocator(alloc);
 
     const addr = std.net.Address.initIp4(.{ 127, 0, 0, 1 }, 7172);
     const client = try std.net.tcpConnectToAddress(addr);
     defer client.close();
 
-    var reader_buf: [2048]u8 = undefined;
+    var reader_buf: [rw_buf_size]u8 = undefined;
     var net_reader = client.reader(&reader_buf);
-    var reader: *std.Io.Reader = net_reader.interface();
+    const reader: *std.Io.Reader = net_reader.interface();
 
-    var writer_buf: [2048]u8 = undefined;
+    var writer_buf: [rw_buf_size]u8 = undefined;
     var net_writer = client.writer(&writer_buf);
-    var writer: *std.Io.Writer = &net_writer.interface;
+    const writer: *std.Io.Writer = &net_writer.interface;
 
-    try writer.writeAll("Hello server");
+    _ = args.next();
+    const cmd = args.next();
+    if (cmd == null) {
+        printhelp();
+    } else if (std.mem.eql(u8, cmd.?, "posts")) {
+        try posts(reader, writer);
+    } else if (std.mem.eql(u8, cmd.?, "auth")) {
+        try auth(reader, writer);
+    } else {
+        printhelp();
+    }
+}
+
+pub fn printhelp() void {
+    print("Invalid command\n", .{});
+}
+
+pub fn posts(reader: *std.Io.Reader, writer: *std.Io.Writer) !void {
+    try writer.writeAll("posts\x00end");
     try writer.flush();
 
-    var msgbuf: [2048]u8 = undefined;
+    var msgbuf: [max_msg_size]u8 = undefined;
     var msgbuf_w: std.Io.Writer = .fixed(&msgbuf);
-    const size = try reader.stream(&msgbuf_w, .limited(msgbuf.len));
-    print("Recieved message: \"{s}\"\n", .{msgbuf[0..size]});
+    var n: usize = 0;
+    while (n == 0) {
+        n = try reader.stream(&msgbuf_w, .limited(msgbuf.len));
+    }
+    print("Recieved message: \"{s}\"\n", .{msgbuf[0..n]});
+}
+
+pub fn auth(reader: *std.Io.Reader, writer: *std.Io.Writer) !void {
+    try writer.writeAll("auth\x00zacoons\x00a\x00end");
+    try writer.flush();
+
+    var msgbuf: [max_msg_size]u8 = undefined;
+    var msgbuf_w: std.Io.Writer = .fixed(&msgbuf);
+    var n: usize = 0;
+    while (n == 0) {
+        n = try reader.stream(&msgbuf_w, .limited(msgbuf.len));
+    }
+    print("Recieved message: \"{s}\"\n", .{msgbuf[0..n]});
 }
