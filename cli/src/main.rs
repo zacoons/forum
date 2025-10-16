bb3c5efd00360dd5597dea5d18ca178a211b6b09
zac <zac@zacoons.com>
2025-10-16 20:18:00 +1000

    Implemented client/server authentication

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use anyhow::Result;
 use futures_util::{SinkExt, StreamExt};
 use tokio::net::TcpStream;
@@ -41,9 +43,27 @@ async fn posts(stream: &mut Stream) -> Result<()> {
 }
 
 async fn auth(stream: &mut Stream) -> Result<()> {
+    let mut stdout = std::io::stdout();
+    stdout.write_all(b"name: ")?;
+    stdout.flush()?;
+    let mut name = String::new();
+    std::io::stdin().read_line(&mut name)?;
+
+    let mut stdout = std::io::stdout();
+    stdout.write_all(b"passwd: ")?;
+    stdout.flush()?;
+    let mut passwd = String::new();
+    std::io::stdin().read_line(&mut passwd)?;
+
     let args = rmpv::Value::Map(vec![
-        (rmpv::Value::from("name"), rmpv::Value::from("zacoons")),
-        (rmpv::Value::from("passwd"), rmpv::Value::from("a")),
+        (
+            rmpv::Value::from("name"),
+            rmpv::Value::from(&name[..name.len() - 1]),
+        ),
+        (
+            rmpv::Value::from("passwd"),
+            rmpv::Value::from(&passwd[..passwd.len() - 1]),
+        ),
     ]);
     let command = rmpv::Value::Map(vec![
         (rmpv::Value::from("cmd"), rmpv::Value::from("auth")),
@@ -52,7 +72,14 @@ async fn auth(stream: &mut Stream) -> Result<()> {
     stream.send(command).await?;
 
     let response = stream.next().await.unwrap()?;
-    println!("{}", response);
+    if let Some(tok) = response["tok"].as_str() {
+        std::fs::write("tok", tok)?;
+        println!("Success");
+    } else if let Some(err) = response["err"].as_str() {
+        println!("Error: {}", err);
+    } else {
+        println!("Unknown error");
+    }
 
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,13 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
 use anyhow::{Result, anyhow};
 use futures_util::{SinkExt, StreamExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio_util::codec::Framed;
-
-type Stream = Framed<TcpStream, msgpack_codec::Codec>;
+use uuid::Uuid;
 
 #[derive(Debug)]
 struct Post {
@@ -22,12 +26,42 @@ struct Reply {
     msg: String,
 }
 
+struct Context {
+    conf: &'static toml::Table,
+    auth_tokens: Arc<Mutex<HashMap<String, String>>>,
+    stream: Framed<TcpStream, msgpack_codec::Codec>,
+}
+
 const RW_BUF_SIZE: usize = 512;
 const MAX_MSG_SIZE: usize = 1024;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:7172").await?;
+    let mut args = std::env::args();
+    _ = args.next();
+    if let Some(cmd) = args.next() {
+        match cmd.as_str() {
+            "passwd" => {
+                let mut passwd = String::new();
+                std::io::stdin().read_line(&mut passwd)?;
+                println!(
+                    "{}",
+                    bcrypt::hash(&passwd[..passwd.len() - 1], bcrypt::DEFAULT_COST)?
+                );
+                return Ok(());
+            }
+            _ => {}
+        }
+    }
+
+    let conf_box = Box::new(toml::from_slice::<toml::Table>(&std::fs::read(
+        "conf.toml",
+    )?)?);
+    let conf = Box::leak(conf_box);
+
+    let auth_tokens = Arc::new(Mutex::new(HashMap::<String, String>::new()));
+
+    let listener = TcpListener::bind(conf["addr"].as_str().unwrap()).await?;
 
     loop {
         let (tcp_stream, _) = listener.accept().await?;
@@ -39,19 +73,24 @@ async fn main() -> Result<()> {
             },
             RW_BUF_SIZE,
         );
-        tokio::spawn(handle_client(stream));
+        let cx = Context {
+            conf: conf,
+            auth_tokens: auth_tokens.clone(),
+            stream: stream,
+        };
+        tokio::spawn(handle_client(cx));
     }
 }
 
-async fn handle_client(mut stream: Stream) {
+async fn handle_client(mut cx: Context) {
     loop {
-        match stream.next().await {
+        match cx.stream.next().await {
             None => {
                 println!("Client disconnected");
                 return;
             }
             Some(Ok(v)) => {
-                handle_cmd(v, &mut stream).await;
+                handle_cmd(v, &mut cx).await;
             }
             Some(Err(err)) => {
                 eprintln!("Error reading from socket: {:?}", err);
@@ -61,27 +100,29 @@ async fn handle_client(mut stream: Stream) {
     }
 }
 
-async fn handle_cmd<'a>(v: rmpv::Value, stream: &mut Stream) {
+async fn handle_cmd<'a>(v: rmpv::Value, cx: &mut Context) {
     if let Some(cmd) = v["cmd"].as_str() {
         if let Err(err) = match cmd {
-            "posts" => handle_posts(stream).await,
+            "posts" => handle_posts(cx).await,
 
-            "auth" => handle_auth(&v["args"], stream).await,
+            "auth" => handle_auth(&v["args"], cx).await,
 
             _ => {
-                _ = stream.send(rmpv::Value::from("Invalid command")).await;
+                _ = cx.stream.send(rmpv::Value::from("Invalid command")).await;
                 eprintln!("Invalid command: {}", cmd);
                 return;
             }
         } {
-            _ = stream
+            _ = cx
+                .stream
                 .send(rmpv::Value::from(err.to_string().as_bytes()))
                 .await;
             eprintln!("{:?}", err);
             return;
         }
     } else {
-        _ = stream
+        _ = cx
+            .stream
             .send(rmpv::Value::from("Badly formatted command"))
             .await;
         eprintln!("Badly formatted command: {}", v);
@@ -89,35 +130,58 @@ async fn handle_cmd<'a>(v: rmpv::Value, stream: &mut Stream) {
     }
 }
 
-// struct ResPosts {
-//     posts: Vec<Post>,
-// }
-async fn handle_posts<'a>(stream: &mut Stream) -> Result<()> {
-    stream.send(rmpv::Value::from("post1, post2, ...")).await?;
-    stream.flush().await?;
+async fn handle_posts<'a>(cx: &mut Context) -> Result<()> {
+    cx.stream
+        .send(rmpv::Value::from("post1, post2, ..."))
+        .await?;
+    cx.stream.flush().await?;
     Ok(())
 }
 
-// struct ArgsAuth {
-//     name: String,
-//     passwd: String,
-// }
-async fn handle_auth<'a>(args: &rmpv::Value, stream: &mut Stream) -> Result<()> {
-    let username = match args["name"].as_str() {
+async fn handle_auth<'a>(args: &rmpv::Value, cx: &mut Context) -> Result<()> {
+    let name = match args["name"].as_str() {
         Some(it) => it,
-        None => return Err(anyhow!("Missing username")),
+        None => return Err(anyhow!("Missing name")),
     };
     let passwd = match args["passwd"].as_str() {
         Some(it) => it,
         None => return Err(anyhow!("Missing password")),
     };
 
-    if username == "zacoons" && passwd == "a" {
-        stream.send(rmpv::Value::from("OK")).await?;
+    if verify_user(name, passwd, cx)? {
+        let tok = Uuid::now_v7().to_string();
+        cx.auth_tokens
+            .lock()
+            .unwrap()
+            .insert(name.to_owned(), tok.clone());
+        cx.stream
+            .send(rmpv::Value::from(vec![(
+                rmpv::Value::from("tok"),
+                rmpv::Value::from(tok),
+            )]))
+            .await?;
     } else {
-        stream.send(rmpv::Value::from("NOAUTH")).await?;
+        cx.stream
+            .send(rmpv::Value::from(vec![(
+                rmpv::Value::from("err"),
+                rmpv::Value::from("not_authenticated"),
+            )]))
+            .await?;
     }
-    stream.flush().await?;
+    cx.stream.flush().await?;
 
     Ok(())
+}
+fn verify_user(name: &str, passwd: &str, cx: &mut Context) -> Result<bool> {
+    if let Some(users) = cx.conf.get("users")
+        && let Some(user) = users.get(name)
+        && let Some(user) = user.as_table()
+    {
+        if let Some(hash) = user.get("passwd")
+            && let Some(hash) = hash.as_str()
+        {
+            return Ok(bcrypt::verify(passwd, hash)?);
+        }
+    }
+    Ok(false)
 }
